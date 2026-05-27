d7a669d1e027e48637294add03dbcd29d5e94b30
Rama Lakshmi <ramu272004@gmail.com>
2026-05-27 17:56:49 +0530

    updated portfolio

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 /* =========================== Typing Animation ================== */
 var typed = new Typed(".typing", {
-    strings: ["Engineer", "Web Designer"],
+    
+    strings: ["Engineer", "Web Designer", "Software Developer 🚀"]
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
